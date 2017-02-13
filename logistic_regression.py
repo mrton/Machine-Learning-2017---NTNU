3fc1f57717031abee4143815a48e84faae083c2a
Ton <mrton@cherdsaks-Mac-mini.local>
2017-02-13 06:12:47 +0100

    update function

--- logistic_regression.py
+++ logistic_regression.py
@@ -13,42 +13,27 @@ def sigmoid(z):
 def compute_cost(loss,X):
     numExamples = X.shape[0]
     for i in range(numExamples):
-        X[0] = X[0]*loss[0]
+        X[i] = X[i]*loss[i]
     cost = X.sum(axis=0)
     return cost.reshape(cost.shape[0],1)
 
+def gradient_descent(X, y, w, n, m, numIterations, learning_rate):
+    for i in range(numIterations):
+        sigma = sigmoid(np.dot(X, w))
+        loss = sigma - y
+        cost = (compute_cost(loss,X))
+        w = w - learning_rate*cost
+        print(w.transpose())
+
+# PARAMETERS
+
 # num of examples
-N = cl_train1.shape[0]
+n = cl_train1.shape[0]
 # num features
 m = cl_train1.shape[1]
-
-X = np.hstack([np.ones((N,1)), cl_train1[0:,:2]])
-y = cl_train1[0:,2:3]
+X = np.hstack([np.ones((n,1)), cl_train1[0:,:m-1]])
+y = cl_train1[0:,m-1:m]
 w = np.ones(m)
-w = w.reshape(3,1)
-alpha = 0.001
-numIterations = 1000
+w = w.reshape(m,1)
 
-sigma = sigmoid(np.dot(X, w))
-loss = sigma - y
-cost = (compute_cost(loss,X))
-w = w - alpha*cost
-print(w)
-
-
-
-
-
-def gradient_descent(X, y, theta, n, numIterations):
-    x_transpose = X.transpose()
-    h = np.dot(x)
-
-'''
-def update(w,learning_rate,X,y,iterations):
-    count = 0
-    while count < iterations:
-
-    w_next = w - np.sum(learning_rate)
-    learning_rate*(sigmoid( w.transpose()* ))
-    count += 1
-'''
+gradient_descent(X, y, w, n, m, numIterations=10000, learning_rate=0.01)
