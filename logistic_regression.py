a089f2a887ea60528d025a90628c912b7a2e545f
Ton <mrton@cherdsaks-Mac-mini.local>
2017-02-13 06:58:37 +0100

    added plot for data points

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,12 +1,10 @@
 import numpy as np
-import random
-
+import matplotlib.pyplot as plt
 
 cl_test1 = np.genfromtxt('./datasets/classification/cl-test-1.csv', delimiter=",")
 cl_test2 = np.genfromtxt('./datasets/classification/cl-test-2.csv', delimiter=",")
 cl_train1 = np.genfromtxt('./datasets/classification/cl-train-1.csv', delimiter=",")
 cl_train2 = np.genfromtxt('./datasets/classification/cl-train-2.csv', delimiter=",")
-
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
@@ -23,10 +21,10 @@ def gradient_descent(X, y, w, n, m, numIterations, learning_rate):
         loss = sigma - y
         cost = (compute_cost(loss,X))
         w = w - learning_rate*cost
-        print(w.transpose())
+        #print(w.transpose())
+    print(w.transpose())
 
 # PARAMETERS
-
 # num of examples
 n = cl_train1.shape[0]
 # num features
@@ -37,3 +35,27 @@ w = np.ones(m)
 w = w.reshape(m,1)
 
 gradient_descent(X, y, w, n, m, numIterations=10000, learning_rate=0.01)
+
+# For plotting
+positive = []
+negative = []
+for example in cl_train1:
+    if example[m-1] == 1:
+        positive.append(example)
+    else:
+        negative.append(example)
+X_pos = []
+Y_pos = []
+X_neg = []
+Y_neg = []
+for i in positive:
+    X_pos.append(i[0])
+    Y_pos.append(i[1])
+for i in negative:
+    X_neg.append(i[0])
+    Y_neg.append(i[1])
+
+plt.plot(X_pos, Y_pos, 'b.')
+plt.plot(X_neg, Y_neg, 'r.')
+plt.axis([0, 1, 0, 1])
+plt.show()
