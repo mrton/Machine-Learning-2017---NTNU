1ed600bde28b4b67f40fd175949380072a02eb9f
Ton <mrton@cherdsaks-Mac-mini.local>
2017-02-14 06:36:00 +0100

    bug fixing, should run correctly now

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,41 +1,62 @@
-import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+import numpy as np
 
 cl_test1 = np.genfromtxt('./datasets/classification/cl-test-1.csv', delimiter=",")
 cl_test2 = np.genfromtxt('./datasets/classification/cl-test-2.csv', delimiter=",")
 cl_train1 = np.genfromtxt('./datasets/classification/cl-train-1.csv', delimiter=",")
 cl_train2 = np.genfromtxt('./datasets/classification/cl-train-2.csv', delimiter=",")
+
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
 def compute_cost(loss,X):
-    numExamples = X.shape[0]
-    for i in range(numExamples):
-        X[i] = X[i]*loss[i]
-    cost = X.sum(axis=0)
-    return cost.reshape(cost.shape[0],1)
+    cost = X*loss
+    cost_sum = cost.sum(0)
+    return cost_sum.reshape(cost_sum.shape[0],1)
 
 def gradient_descent(X, y, w, n, m, numIterations, learning_rate):
+    error_ce = []
     for i in range(numIterations):
         sigma = sigmoid(np.dot(X, w))
         loss = sigma - y
-        cost = (compute_cost(loss,X))
+        error_ce.append(cross_entropy_error(n,y,sigma))
+        cost = compute_cost(loss,X)
         w = w - learning_rate*cost
-        #print(w.transpose())
-    print(w.transpose())
+    return (w.T,error_ce)
 
-# PARAMETERS
-# num of examples
+def predict(w,X,y):
+    z = np.dot(X,w.T)
+    sigma = sigmoid(z)
+    predict = np.array(list(map(lambda x : 1 if x>0.5 else 0, sigma )))
+    n = y.shape[0]
+    score = 0
+    for i in range(n):
+        if y[i][0] == predict[i]:
+            score += 1
+    print("scored: "+ str((score/n)*100)+"%")
+
+
+def cross_entropy_error(n,y,sigma):
+    return(-(1/n)*np.sum(y*np.log(sigma) + (1-y)*np.log(1-sigma)))
+
+
 n = cl_train1.shape[0]
-# num features
 m = cl_train1.shape[1]
 X = np.hstack([np.ones((n,1)), cl_train1[0:,:m-1]])
 y = cl_train1[0:,m-1:m]
 w = np.ones(m)
 w = w.reshape(m,1)
+w , e = gradient_descent(X, y, w, n, m, numIterations=1000, learning_rate=0.01)
 
-gradient_descent(X, y, w, n, m, numIterations=10000, learning_rate=0.01)
 
+n = cl_test1.shape[0]
+m = cl_test1.shape[1]
+X_test1 = np.hstack([np.ones((n,1)), cl_test1[0:,:m-1]])
+y_test1 = cl_test1[0:,m-1:m]
+predict(w,X_test1,y_test1)
+
+'''
 # For plotting
 positive = []
 negative = []
@@ -44,18 +65,41 @@ for example in cl_train1:
         positive.append(example)
     else:
         negative.append(example)
-X_pos = []
+
+X1_pos = []
+X2_pos = []
+X1_neg = []
+X2_neg = []
 Y_pos = []
-X_neg = []
 Y_neg = []
 for i in positive:
-    X_pos.append(i[0])
-    Y_pos.append(i[1])
-for i in negative:
-    X_neg.append(i[0])
-    Y_neg.append(i[1])
+    X1_pos.append(i[0])
+    X2_pos.append(i[1])
+    Y_pos.append(1)
 
-plt.plot(X_pos, Y_pos, 'b.')
-plt.plot(X_neg, Y_neg, 'r.')
+for i in negative:
+    X1_neg.append(i[0])
+    X2_neg.append(i[1])
+    Y_neg.append(0)
+
+plt.plot(X1_pos, X2_pos, 'b.')
+plt.plot(X1_neg, X2_neg, 'r.')
 plt.axis([0, 1, 0, 1])
 plt.show()
+
+x1 = cl_train1[:,[0]]
+x2 = cl_train1[:,[1]]
+y = cl_train1[:,[2]]
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(X1_pos, X2_pos, Y_pos, c='b', marker='o')
+ax.scatter(X1_neg, X2_neg, Y_neg, c='r', marker='o')
+
+
+ax.set_xlabel('X1')
+ax.set_ylabel('X2')
+ax.set_zlabel('Y')
+
+plt.show()
+'''
--- test.py
+++ test.py
@@ -1,70 +1,32 @@
-import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
-
-# REGRESSION
-
-test_1d = np.genfromtxt('./datasets/regression/reg-1d-test.csv', delimiter=",")
-train_1d = np.genfromtxt('./datasets/regression/reg-1d-train.csv', delimiter=",")
-#test_2d = np.genfromtxt('./datasets/regression/reg-2d-test.csv', delimiter=",")
-#train_2d = np.genfromtxt('./datasets/regression/reg-2d-train.csv', delimiter=",")
-
-'''
-X_train_2d = train_2d[0:,:2]
-y_train_2d = train_2d[0:,2:3]
-N_2d = y_train_2d.size
-X_train_2d = np.hstack([ np.ones((N_2d,1)) , X_train_2d])
-w = calc_weights(X_train_2d, y_train_2d)
-
-X_train_1d = train_2d[0:,:1]
-y_train_1d = train_2d[0:,1:2]
-N_1d = y_train_1d.size
-X_train_1d = np.hstack([ np.ones((N_1d,1)) , X_train_1d])
-w = calc_weights(X_train_1d, y_train_1d)
-print(w)
-'''
-
-X,Y = np.hsplit(test_1d,2)
-print(X)
-
-# For display in plot
-x = []
-y = []
-for i in test_1d:
-    x.append(i[0])
-    y.append(i[1])
-
-def calc_weights(X,y):
-    X_t = X.transpose()
-    w = np.matmul( np.linalg.pinv(np.matmul(X_t,X)) , np.matmul(X_t,y) )
-    return w
+import numpy as np
 
 
-def mean_square_error(w,X,y):
-    # Should be the number of examples
-    N = X.size / 2
-    e = (1 / N) * np.power( np.absolute(np.matmul(X,w) - y) , 2)
-    return e
+def randrange(n, vmin, vmax):
+    '''
+    Helper function to make an array of random numbers having shape (n, )
+    with each number distributed Uniform(vmin, vmax).
+    '''
+    return (vmax - vmin)*np.random.rand(n) + vmin
 
-ones = np.ones((240,1))
-new_X = np.hstack([ones, X])
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
-w = calc_weights(X,Y)
-bias = np.array([[1]])
-w = np.concatenate((bias,w))
+n = 100
 
-mse = (w,new_X,y)
+# For each set of style and range settings, plot n random points in the box
+# defined by x in [23, 32], y in [0, 100], z in [zlow, zhigh].
+for c, m, zlow, zhigh in [('r', 'o', -50, -25), ('b', '^', -30, -5)]:
+    xs = randrange(n, 23, 32)
+    ys = randrange(n, 0, 100)
+    zs = randrange(n, zlow, zhigh)
+    ax.scatter(xs, ys, zs, c=c, marker=m)
 
-print(w)
+print(type(xs))
 
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
 
-
-plt.plot(x, y, 'b.')
-plt.axis([0, 1, 0, 1])
 plt.show()
-
-
-
-
-
-
-#w = np.insert(w, 0, 10, axis=0)
